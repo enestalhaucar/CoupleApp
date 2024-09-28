db3d3137c62eb03ff078bdb9ad34adda9e6b64fe
Enes Talha Uçar <talhauenes@gmail.com>
2024-09-28 19:48:10 +0300

    Project Initialized

---
+++ CoupleApp/CoupleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoupleAppApp.swift
+//  CoupleApp
+//
+//  Created by Enes Talha Uçar  on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoupleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
